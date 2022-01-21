db0ab969405d4e5a52837877bfca5e870da27be5
daniel bobadilla <danybavila@gmail.com>
2022-01-21 11:15:57 -0500

    status_codes

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from pydantic import Field
 # FastAPI
 from fastapi import FastAPI
 from fastapi import Body, Query, Path
+from fastapi import status
 
 app = FastAPI()
 
@@ -87,19 +88,29 @@ class PersonOut(PersonBase):
     #        }
     #    }
 
-@app.get("/")
+@app.get(
+    path="/",
+    status_code=status.HTTP_200_OK
+    )
 def home():
     return {"Hello": "World!"}
 
 # Request and Response Body
 
-@app.post("/person/new", response_model=PersonOut)
+@app.post(
+    path="/person/new",
+    response_model=PersonOut,
+    status_code=status.HTTP_201_CREATED
+    )
 def create_person(person: Person = Body(...)):
     return person
 
 # Validaciones: Query Parameters
 
-@app.get("/person/detail")
+@app.get(
+    path="/person/detail",
+    status_code=status.HTTP_200_OK
+    )
 def show_person(
     name: Optional[str] = Query(
         None, min_length=1,
