a546016d6d7ffb4647f667ed4a64b994048defab
daniel bobadilla <danybavila@gmail.com>
2022-01-21 10:37:12 -0500

    automatic_path_and_query_examples

--- main.py
+++ main.py
@@ -1,105 +1,139 @@
-#Python
-from argparse import OPTIONAL
+# Python
 from typing import Optional
 from enum import Enum
-#Pydantic
+
+# Pydantic
 from pydantic import BaseModel
 from pydantic import Field
-#FastApi
-from fastapi import FastAPI, Query
+
+# FastAPI
+from fastapi import FastAPI
 from fastapi import Body, Query, Path
 
 app = FastAPI()
 
-#Models
-class HairColor(Enum):
-    brown = "brown"
-    white = "white"
-    black = 'black'
-    blonde = 'blonde'
-    red = 'red'
+# Models
 
+class HairColor(Enum):
+    white = "white"
+    brown = "brown"
+    black = "black"
+    blonde = "blonde"
+    red = "red"
 
 class Location(BaseModel):
-    city: str
-    state : str
-    country : str
+    city: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        description="The city where the person lives",
+        example="New York",
+    )
+    state: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        description="The state where the person lives",
+        example="New York",
+    )
+    country: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        description="The country where the person lives",
+        example="United States",
+    )
 
 class Person(BaseModel):
     first_name: str = Field(
         ...,
         min_length=1,
-        max_length=50
+        max_length=50,
+        example="Miguel"
         )
     last_name: str = Field(
         ...,
         min_length=1,
-        max_length=50
+        max_length=50,
+        example="Gonzalez"
         )
     age: int = Field(
         ...,
-        ge=0,
-        le=150
+        gt=0,
+        le=115,
+        example=25
     )
-    hair_color: Optional[HairColor] = Field(default=None)
-    is_maried: Optional[bool] = Field(default=None)
+    hair_color: Optional[HairColor  ] = Field(default=None, example=HairColor.brown)
+    is_married: Optional[bool] = Field(default=None, example=False)
 
+    #class Config:
+    #    schema_extra = {
+    #        "example": {
+    #            "first_name": "Facundo",
+    #            "last_name": "García Martoni",
+    #            "age": 29,
+    #            "hair_color": "blonde",
+    #            "is_married": False,
+    #        }
+    #    }
 
 @app.get("/")
 def home():
-    return {"message": "Hello World"}
+    return {"Hello": "World!"}
 
-#request and response body
+# Request and Response Body
 
 @app.post("/person/new")
 def create_person(person: Person = Body(...)):
     return person
 
-#Validaciones: Query Parameters
+# Validaciones: Query Parameters
 
 @app.get("/person/detail")
 def show_person(
     name: Optional[str] = Query(
-        None,
-        min_length=1,
+        None, min_length=1,
         max_length=50,
-        title = "Person Name",
-        description="This is the person name. Its between 1 and 50 characters",
+        title="Person Name",
+        description="This is the person name,  It's between 1 and 50 characters",
+        example="Rocio"
         ),
-    age:Optional[str] = Query(
+    age: str = Query(
         ...,
         title="Person Age",
-        description="This is the person age. It's required",
+        description="This is the person age, It's required",
+        example="25"
         )
 ):
     return {name: age}
 
-#Validaciones: Path Parameters
+# Validaciones Path Paremeters
 
 @app.get("/person/detail/{person_id}")
 def show_person(
     person_id: int = Path(
-        ..., 
+        ...,
         gt=0,
-        title="Person ID",
-        description="This is the person id. It's required",
-        )
+        example=123
+    )
 ):
-    return {person_id: "It exists"}
+    return {person_id: "It exists!"}
 
-#validaciones: request body
+# Validaciones: Request Body
 
 @app.put("/person/{person_id}")
 def update_person(
     person_id: int = Path(
-        ..., 
+        ...,
         title="Person ID",
-        description="This is the person id. It's required",
-        gt=0
-        ),
+        description="This is the person ID",
+        gt=0,
+        example=123
+    ),
     person: Person = Body(...),
-    Location: Location = Body(...)
+    location: Location = Body(...)
 ):
-    results = person.dict(),
-    results.update(Location.dict())
-    return results
+    #results = person.dict()
+    #results.update(location.dict())
+    #return results
+    return person 
