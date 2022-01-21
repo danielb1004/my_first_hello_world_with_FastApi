80a437dd7c81a5e9532289d33e5eac6965835b54
daniel bobadilla <danybavila@gmail.com>
2022-01-21 12:46:29 -0500

    files

--- main.py
+++ main.py
@@ -193,3 +193,17 @@ def contact(
     ads:Optional[str] = Cookie(default=None)
 ):
     return user_agent
+
+#Files
+
+@app.post(
+    path="/post-image"
+)
+def post_image(
+    image: UploadFile = File(...),
+):
+    return {
+        "Filename": image.filename,
+        "Format":image.content_type,
+        "Size(kb)":round(len(image.file.read())/1024, ndigits=2)
+    }
